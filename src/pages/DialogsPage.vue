2c47200a751206f3f7c49e6e0c1ca60dffd7f769
Boris Muñoz <bmunoz89@users.noreply.github.com>
2023-07-19 15:06:17 -0400

    Display prompt response

--- src/pages/DialogsPage.vue
+++ src/pages/DialogsPage.vue
@@ -4,6 +4,11 @@
 
         <q-separator spaced inset />
 
+        <template v-if="name">
+            <span class="text-h6">{{ name }}</span>
+            <q-separator spaced inset />
+        </template>
+
         <div class="q-pa-md q-gutter-sm">
             <q-btn color="primary" label="Alert" @click="alert"/>
             <q-btn color="primary" label="Confirm" @click="confirm"/>
@@ -14,6 +19,7 @@
 
 <script setup lang="ts">
 import { useQuasar } from 'quasar';
+import { ref } from 'vue';
 
 const $q = useQuasar();
 
@@ -25,20 +31,18 @@ const alert = () => {
 }
 
 const confirm = () => {
-    const dialog = $q.dialog({
+    $q.dialog({
         title: 'Confirm',
         message: 'Would you like to turn on the wifi?',
         cancel: true,
         persistent: true
     });
-
-    dialog.onOk((payload) => {
-        alert(payload);
-    });
 }
 
+const name = ref();
+
 const prompt = () => {
-    $q.dialog({
+    const prompt = $q.dialog({
         title: 'Prompt',
         message: 'What is your name?',
         cancel: true,
@@ -47,6 +51,10 @@ const prompt = () => {
             model: '',
             type: 'text'
         }
+    });
+
+    prompt.onOk((payloadName: string) => {
+        name.value = payloadName;
     });
 }
 
